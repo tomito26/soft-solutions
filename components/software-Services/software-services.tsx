c988e5655c3933c23ab351b67c594d6c962d0b6f
tomito26 <tommybwah@gmail.com>
2024-11-12 14:49:08 +0300

    style: made the services responsive

--- components/software-Services/software-services.tsx
+++ components/software-Services/software-services.tsx
@@ -15,17 +15,17 @@ const SoftwareServices = () => {
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
         }}
-        className="flex items-center md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[160px] 3xl:px-[200px] 4xl:px-[240px] 5xl:px-[320px]"
+        className="flex items-center px-4 md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[160px] 3xl:px-[200px] 4xl:px-[240px] 5xl:px-[320px]"
       >
-        <div className="w-8/12">
-          <h1 className="font-bold text-6xl text-white pb-6">
+        <div className="md:w-8/12">
+          <h1 className="font-bold text-3xl md:text-6xl text-white pb-6">
             Software Services
           </h1>
         </div>
       </div>
-      <div className="pt-16 pb-24 md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[160px] 3xl:px-[200px] 4xl:px-[240px] 5xl:px-[320px]">
-        <div className="flex items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+      <div className="px-4 pt-10 md:pt-16 pb-24 md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[160px] 3xl:px-[200px] 4xl:px-[240px] 5xl:px-[320px]">
+        <div className="flex md:flex-row flex-col-reverse items-center gap-4 md:gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/side-view-man-working-with-computer.jpg"
               alt=" a guy holding written design of website"
@@ -33,11 +33,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6 md:mt-0">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               Web and Mobile Applications Development
             </h2>
-            <p className="text-base ">
+            <p className="md:text-base text-sm ">
               Our team specializes in creating custom software applications that
               align with your business goals. From mobile apps to complex web
               platforms, we provide solutions that are not only functional but
@@ -48,8 +48,8 @@ const SoftwareServices = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-row-reverse items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+        <div className="flex flex-col-reverse md:flex-row-reverse items-center gap-4 md:gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/system-design.jpg"
               alt=" a guy holding written design of website"
@@ -57,11 +57,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6 md:mt-0">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               System Design & Integration
             </h2>
-            <p className="text-base ">
+            <p className="md:text-base text-sm">
               With our expertise in system architecture and integration, we
               simplify complex processes by designing solutions that bring your
               systems together. Our services streamline workflows, improve data
@@ -73,8 +73,8 @@ const SoftwareServices = () => {
             </p>
           </div>
         </div>
-        <div className="flex  items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+        <div className="flex flex-col-reverse md:flex-row  items-center gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/saas-concept-collage.jpg"
               alt=" a guy holding written design of website"
@@ -82,11 +82,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6 md:mt-0">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               Cloud Services
             </h2>
-            <p className="text-base">
+            <p className="md:text-base text-sm">
               Our cloud services are designed to help businesses make the most
               of the cloud. We provide secure storage, data migration, and cloud
               management solutions that enable flexibility, cost savings, and
@@ -97,8 +97,8 @@ const SoftwareServices = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-row-reverse  items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+        <div className="flex flex-col-reverse md:flex-row-reverse  items-center gap-4 md:gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/female-scientist-white.jpg"
               alt=" a guy holding written design of website"
@@ -106,11 +106,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               Cybersecurity
             </h2>
-            <p className="text-base">
+            <p className="md:text-base text-sm">
               In today&apos;s digital landscape, cybersecurity is a priority,
               and we offer comprehensive solutions to protect your business. Our
               services include threat assessments, firewall management, data
@@ -123,8 +123,8 @@ const SoftwareServices = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-row items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+        <div className="flex flex-col-reverse md:flex-row items-center gap-4 md:gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/young-businesswoman-working-with-statistics-charts-computer-monitor-analyzing-company-data-paperwork-using-online-information-create-research-report-before-deadline-night-after-hours.jpg"
               alt=" a guy holding written design of website"
@@ -132,11 +132,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6 md:mt-0">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               Data Management
             </h2>
-            <p className="text-base">
+            <p className="text-sm md:text-base">
               We empower you to harness the full
               potential of your data. We offer data analytics, database
               management, and processing solutions that convert raw data into
@@ -148,8 +148,8 @@ const SoftwareServices = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-row-reverse items-center gap-12">
-          <div className="h-[328px] w-[500px] relative shrink-0">
+        <div className="flex flex-col-reverse md:flex-row-reverse items-center gap-4 md:gap-12">
+          <div className="md:h-[328px] h-[240px] w-full md:w-[500px] relative shrink-0">
             <Image
               src="/assets/it-specialist-looking-network-issues-affecting-rigs-performance-using-tablet.jpg"
               alt=" a guy holding written design of website"
@@ -157,11 +157,11 @@ const SoftwareServices = () => {
               className="absolute object-cover"
             />
           </div>
-          <div className="">
-            <h2 className="text-5xl font-bold text-monochrome mb-4">
+          <div className="mt-6 md:mt-0">
+            <h2 className="text-2xl md:text-5xl font-bold text-monochrome mb-4">
               IT Consulting
             </h2>
-            <p className="text-base">
+            <p className="text-sm md:text-base">
               We provide businesses with expert guidance
               on their technology strategy. We work closely with you to
               understand your challenges, goals, and vision, offering insights
@@ -174,11 +174,11 @@ const SoftwareServices = () => {
           </div>
         </div>
         <div className="pt-16 pb-16 mt-16 flex justify-center items-center flex-col">
-          <div className="w-8/12">
-            <h2 className="font-bold text-5xl text-center mb-4 capitalize text-monochrome">
+          <div className="md:w-8/12">
+            <h2 className="font-bold text-2xl md:text-5xl text-center mb-4 capitalize text-monochrome">
               Ready to get Started?
             </h2>
-            <p className="font-medium text-center mb-6">
+            <p className="font-medium text-sm md:text-base text-center mb-6">
               Transform your business with cutting-edge software solutions
               designed to fit your unique needs. From custom applications to
               cloud services and cybersecurity, we&apos;re here to drive your
